68d723c37f55fe88e910b08dcfbea00804a63fba
saalimh <59394790+saalimh@users.noreply.github.com>
2024-11-30 14:34:27 +0000

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Saalim Al Marhubi on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
